236faaa6e0a1adf55c21f332f9ead0702287f376
Trim21 <trim21me@gmail.com>
2017-11-18 05:09:44 +0800

    get all env in config

--- app/app.py
+++ app/app.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask
 # flask-peewee bindings
 from flask_peewee.db import Database
+from config import ppoi_secret
 
 # from flask_login import LoginManager
 
-ppoi_secret = os.getenv('ppoi_key')
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
 
